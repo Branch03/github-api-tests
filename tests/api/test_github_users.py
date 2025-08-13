a0dec680151d6fd7a4b686b5e9c8ba27920981eb
Branch03 <171587652+Branch03@users.noreply.github.com>
2025-08-13 23:33:11 +0400

    Simplified the code

--- tests/api/test_github_users.py
+++ tests/api/test_github_users.py
@@ -1,16 +1,15 @@
 import pytest
 
-@pytest.mark.parametrize('username', ["octocat", "torvalds", "mojombo"])
+USERNAME_LIST = ["octocat", "torvalds", "mojombo"]
+
+@pytest.mark.parametrize('username', USERNAME_LIST)
 def test_username(base_url, session, username):
-    url = base_url + "/users/" + username
-    r = session.get(url)
+    r = session.get(f"{base_url}/users/{username}")
     assert r.status_code == 200
     data = r.json()
     assert "login" in data 
     assert data["login"] == username
 
 def test_user_not_found(base_url, session,):
-    url = base_url + "/users/" + "chmo5839287822"
-    r = session.get(url)
+    r = session.get(f"{base_url}/users/{"chmo7741941"}")
     assert r.status_code == 404
-    
